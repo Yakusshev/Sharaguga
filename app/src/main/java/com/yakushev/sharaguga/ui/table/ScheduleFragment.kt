94b3209b2dd59ee6cf415f709949555dfe8b9d35
Yakushev <Yakushev@users.noreply.github.com>
2022-09-18 00:29:30 +0300

    Добавил даты в TabLayout, исправил statusBarColor

--- app/src/main/java/com/yakushev/sharaguga/ui/table/ScheduleFragment.kt
+++ app/src/main/java/com/yakushev/sharaguga/ui/table/ScheduleFragment.kt
@@ -2,9 +2,11 @@ package com.yakushev.sharaguga.ui.table
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -15,6 +17,7 @@ import com.yakushev.sharaguga.R
 import com.yakushev.sharaguga.databinding.FragmentScheduleBinding
 import com.yakushev.sharaguga.ui.adapters.SchedulePagerAdapter
 import java.time.LocalDate
+import java.util.*
 
 class ScheduleFragment : Fragment() {
 
@@ -44,6 +47,7 @@ class ScheduleFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O) //TODO remove
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -52,29 +56,37 @@ class ScheduleFragment : Fragment() {
         val adapter = SchedulePagerAdapter(this)
         binding.viewPager.adapter = adapter
 
-        //binding.viewPager.currentItem = 1
+        val day = LocalDate.now().dayOfWeek.value
+        binding.viewPager.currentItem = day - 1
+
+        //val asd = LocalDate.now()
+
+        val array = getDayOfMonthArray()
 
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             this.position = position
-            when (position + 1) {
-                1 -> tab.text = getString(R.string.tab_layout_text_monday)
-                2 -> tab.text = getString(R.string.tab_layout_text_tuesday)
-                3 -> tab.text = getString(R.string.tab_layout_text_wednesday)
-                4 -> tab.text = getString(R.string.tab_layout_text_thursday)
-                5 -> tab.text = getString(R.string.tab_layout_text_friday)
-                6 -> tab.text = getString(R.string.tab_layout_text_saturday)
-                7 -> tab.text = getString(R.string.tab_layout_text_sunday)
-            }
+            val dayOfMonth = array[position].toString()
+            val dayOfWeek = resources.getStringArray(R.array.tab_layout)[position]
+            tab.text = dayOfMonth + "\n" + dayOfWeek
         }.attach()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val day = LocalDate.now().dayOfWeek.value
-            binding.viewPager.currentItem = day - 1
-        }
 
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_schedule_to_add_fragment)
         }
+    }
+
+    private fun getDayOfMonthArray(): IntArray {
+        val calendar = Calendar.getInstance()
+
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.firstDayOfWeek)
+
+        val array = IntArray(7)
+
+        for (i in array.indices) {
+            array[i] = calendar[Calendar.DAY_OF_MONTH]
+            calendar.add(Calendar.DAY_OF_MONTH, 1)
+        }
+        return array
     }
 
     private fun setActionBarTitle() {
