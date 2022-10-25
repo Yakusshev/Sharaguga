45fe5d46b01728c356da7a8f51d5a9ac9ccfc2bc
Yakushev <Yakushev@users.noreply.github.com>
2022-10-26 00:40:16 +0300

    Test

--- domain/src/main/java/com/yakushev/domain/usecase/TimeScheduleUseCase.kt
+++ domain/src/main/java/com/yakushev/domain/usecase/TimeScheduleUseCase.kt
@@ -1,15 +1,12 @@
 package com.yakushev.domain.usecase
 
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.yakushev.domain.models.schedule.TimeCustom
 import com.yakushev.domain.repository.Repository
 
 class TimeScheduleUseCase(private val repository: Repository<TimeCustom>) {
 
-    suspend fun get(univerPath: String) : List<TimeCustom> {
-        val semesterReference = Firebase.firestore.document(univerPath)
-        return repository.get(semesterReference)
+    suspend fun get(path: String) : List<TimeCustom> {
+        return repository.get(path)
     }
 
 }
